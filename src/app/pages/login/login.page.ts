ece669ea57e973eeda5c751e45817eed581e9bbb
Andy Loor <andiloor2809@gmail.com>
2023-06-19 01:11:01 -0500

    login implementado con stilos fallidos

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,35 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  templateUrl: 'login.page.html',
+  styleUrls: ['login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage implements OnInit{
+
   form: FormGroup;
-  isTypePassword: boolean = true;
-  islogin = false;
-
-
+  isTypePasswordisLogin: boolean = true;
+isLogin=false;
+  isTypePassword: any;
   constructor() {
     this.initForm();
   }
   ngOnInit(): void {
-
+   
   }
+
   initForm() {
     this.form = new FormGroup({
-
-      email: new FormGroup('',
-        { validators: [Validators.required, Validators.email] }),
-
-      password: new FormControl('',
-        { validators: [Validators.required, Validators.minLength(8)] }),
-
-
-    })
+      email: new FormControl('', 
+        {validators: [Validators.required, Validators.email]}
+      ),
+      password: new FormControl('', 
+        {validators: [Validators.required, Validators.minLength(8)]}
+      ),
+    });
   }
 
+  onChange() {
+    this.isTypePassword = !this.isTypePassword;
+  }
+
+  onSubmit() {
+    if(!this.form.valid) return;
+    console.log(this.form.value);
+  }
 
 }
